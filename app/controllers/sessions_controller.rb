c9fe2aec60cbad94fca025d0026bdb5232a78ed0
+ <+>
2021-11-16 19:34:49 -0600

    first commit to navbar branch

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
     def create #login
         user = User.find_by(email: params[:email])
         if user&.authenticate(params[:password])
-            session[user] = user.id
+            session[user] = user.id #authenticating 
             render json: user
         else 
             render json: { errors: "Invalid username or password"}, status: :unauthorized
