ed03d1692fbf64fd6ead11aaddb44732b2b8291d
+ <+>
2021-11-18 00:13:28 -0600

    error messages working for login/signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id #authenticating 
             render json: user
         else 
-            render json: { errors: "Invalid username or password"}, status: :unauthorized
+            render json: { errors: "Invalid username or password. Please try again."}, status: :unauthorized
         end
     end 
 
