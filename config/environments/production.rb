3ef705c1e78cd8df84b430dcce55ceb7573c8ec7
QuangDo <qdangdo@gmail.com>
2021-11-20 18:39:11 -0600

    Config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://chat-app-fi.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://chat-app-fi.herokuapp.com', 'https://chat-app-fi.herokuapp.com' ]
+  config.action_cable.allowed_request_origins = [ 'https://chat-app-fi.herokuapp.com', /http:\/\/chat-app-fi.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
